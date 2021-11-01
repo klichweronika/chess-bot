10fe8f47bcec6ecad33e0995dcf89069a1abe921
Weronika Klich <klichweronika9@gmail.com>
2021-11-01 21:39:56 +0100

    Add chess game remove, fix info status

---
+++ main.js
@@ -0,0 +1,132 @@
+let board = null;
+let game = new Chess();
+let $status = $("#status");
+let $fen = $("#fen");
+let $pgn = $("#pgn");
+let whiteSquareGrey = "#a9a9a9";
+let blackSquareGrey = "#696969";
+
+function onDragStart(source, piece, position, orientation) {
+  if (game.game_over()) return false;
+
+  if (
+    (game.turn() === "w" && piece.search(/^b/) !== -1) ||
+    (game.turn() === "b" && piece.search(/^w/) !== -1)
+  ) {
+    return false;
+  }
+}
+
+function makeRandomMove() {
+  let possibleMoves = game.moves();
+
+  if (possibleMoves.length === 0) return;
+
+  let randomIdx = Math.floor(Math.random() * possibleMoves.length);
+  game.move(possibleMoves[randomIdx]);
+  board.position(game.fen());
+  updateStatus();
+}
+
+function onDrop(source, target) {
+  let move = game.move({
+    from: source,
+    to: target,
+    promotion: "q",
+  });
+
+  if (move === null) return "snapback";
+
+  // make random legal move for black
+  window.setTimeout(makeRandomMove, 1000);
+  updateStatus();
+}
+
+function onSnapEnd() {
+  board.position(game.fen());
+}
+
+function updateStatus() {
+  var status = "";
+
+  var moveColor = "White";
+  if (game.turn() === "b") {
+    moveColor = "Black";
+  }
+
+  if (game.in_checkmate()) {
+    status = "Game over, " + moveColor + " is in checkmate.";
+  } else if (game.in_draw()) {
+    status = "Game over, drawn position";
+  } else {
+    status = moveColor + " to move";
+    if (game.in_check()) {
+      status += ", " + moveColor + " is in check";
+    }
+  }
+
+  $status.html(status);
+  console.log();
+  $fen.html(JSON.stringify(game.history({ verbose: true })));
+  $pgn.html(game.pgn());
+}
+
+function removeGreySquares() {
+  $("#myBoard .square-55d63").css("background", "");
+}
+
+function greySquare(square) {
+  var $square = $("#myBoard .square-" + square);
+
+  var background = whiteSquareGrey;
+  if ($square.hasClass("black-3c85d")) {
+    background = blackSquareGrey;
+  }
+
+  $square.css("background", background);
+}
+
+function onMouseoverSquare(square, piece) {
+  // get list of possible moves for this square
+  var moves = game.moves({
+    square: square,
+    verbose: true,
+  });
+
+  // exit if there are no moves available for this square
+  if (moves.length === 0) return;
+
+  // highlight the square they moused over
+  greySquare(square);
+
+  // highlight the possible squares for this piece
+  for (var i = 0; i < moves.length; i++) {
+    greySquare(moves[i].to);
+  }
+}
+
+function onMouseoutSquare(square, piece) {
+  removeGreySquares();
+}
+
+function onSnapEnd() {
+  board.position(game.fen());
+}
+
+let config = {
+  draggable: true,
+  position: "start",
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd,
+  onMouseoutSquare: onMouseoutSquare,
+  onMouseoverSquare: onMouseoverSquare,
+  onSnapEnd: onSnapEnd,
+};
+board = Chessboard("myBoard", config);
+
+$("#resetButton").on("click", (_) => {
+  board.start();
+  game.reset();
+  $pgn.empty();
+});
